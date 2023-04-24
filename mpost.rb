7d549675eabccf9f1fbeff9df7e629cc43b79662
志村 <shimura-hir@obun.biz>
2023-04-24 22:19:23 +0900

    IE このブラウザは現在サポートされていません。

--- mpost.rb
+++ mpost.rb
@@ -1,4 +1,21 @@
 require 'win32ole'
 
-class WIN32OLE; def oms = ole_methods.map{|m|m.name}.sort; end
-Edge = WIN32OLE.connect 'Edge.Application'
+
+class WIN32OLE; def oms = ole_methods.map(&:name).sort; end
+#Ie = WIN32OLE.connect 'InternetExplorer.Application'
+ie = WIN32OLE.new 'InternetExplorer.Application'
+begin
+  ie.visible = true
+  #ie.oms.display
+  ie.Navigate(
+    'https://twitter.com/home',
+    0,
+    '_self',
+    nil,
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0'
+  )
+  sleep 5
+ensure
+  sleep 1 while ie.Busy
+  ie.quit
+end
