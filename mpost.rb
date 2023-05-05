ededfa7608a0a5b9116493f13aadb8a59c90c9ef
志村 <shimura-hir@obun.biz>
2023-05-05 18:07:45 +0900

    tiwtter with authenticated cookie values in ../cookies.json

--- mpost.rb
+++ mpost.rb
@@ -3,8 +3,18 @@ require 'selenium-webdriver'
 Selenium::WebDriver::Edge::Service.driver_path \
   = File.join '..\edgedriver.112.0.1722.39\edgedriver_win64', 'msedgedriver.exe'
 
+urls = {
+  twitter: 'https://twitter.com/home',
+}
+cookies = JSON File.read('../cookies.json'), symbolize_names: true
+
 driver = Selenium::WebDriver.for :edge
-driver.get("https://www.google.com/")
+driver.get "https://www.google.com/"
+
+driver.get urls[:twitter]
+cookies[:twitter].each{ driver.manage.add_cookie _1 }
+driver.get urls[:twitter]
+sleep 10
 
 driver.quit
 
